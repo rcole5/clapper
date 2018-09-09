9829f7469ced8ffb3572011f3017c3e1e7d7716b
Ryan Cole <ryan.cole@bkvfoundry.com>
2018-09-09 20:59:19 +1000

    👏 Fix 👏 an 👏 oopsie 👏

--- clapper.go
+++ clapper.go
@@ -1,24 +1,24 @@
 package clapper
 
 import (
-	"os"
 	"strings"
 )
 
 // Clappify a string
 func Clap(victim string) (result string) {
 	// Get the length of the clap
-	length := len(strings.Split(victim, " "))
+	arr := strings.Split(victim, " ")
+	length := len(arr)
 
 	if length == 1 {
 		// When we have one word clap between letters
 		result = "👏 "
-		for _, char := range os.Args[1] {
+		for _, char := range arr {
 			result = result + string(char) + " 👏 "
 		}
 	} else {
 		// If we have a sentence then clap between words
-		str := strings.Join(os.Args[1:], " ")
+		str := strings.Join(arr, " ")
 		result = strings.Replace(str, " ", " 👏 ", -1)
 		result = "👏 " + result + " 👏"
 	}
